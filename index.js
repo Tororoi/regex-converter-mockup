22bd4a0efbe498bb165cd81ac4fa369dd7d6e8a4
Tororoi <cantwell.tom@gmail.com>
2020-11-28 10:58:49 -0500

    regex validates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let before = document.getElementById("before");
 let formula = document.getElementById("regex");
 let after = document.getElementById("after");
 
-// let regex = /./g
+// let regex = /[^hl]+/g
 // formula.value = regex;
 
 before.addEventListener("input", forwardConvert);
@@ -12,14 +12,39 @@ formula.addEventListener("input", updateRegex);
 after.addEventListener("input", backwardConvert);
 
 function forwardConvert(e) {
-    after.value = before.value.match(regex);
+    console.log(before.value.match(regex))
+        if (regex.test(before.value)) {
+        after.value = before.value.match(regex).join("");
+    } else {
+        after.value = "";
+    }
 }
 
 function updateRegex(e) {
-    regex = RegExp(e.target.value);
-    after.value = before.value.match(regex);
+    //validate regex first
+    try {RegExp(e.target.value,"g")}
+    catch(err) {
+        formula.style.color = "red";
+        return;
+    }
+    //it's valid, so continue
+    regex = RegExp(e.target.value,"g");
+    formula.style.color = "black";
+
+    //use empty string if null result
+    if (regex.test(before.value)) {
+        after.value = before.value.match(regex).join("");
+    } else {
+        after.value = "";
+    }
+
+    console.log(after.value)
 }
 
 function backwardConvert(e) {
-    
+    //The hard part
+    //compare before and after
+
+    //using differences, determine appropriate regex 
+    //update formula to reflect new regex
 }
