9f7cf6b55c1a9a2f82ecf0671958313b165f2f2f
Tororoi <cantwell.tom@gmail.com>
2020-11-28 12:54:32 -0500

    ...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let before = document.getElementById("before");
 let formula = document.getElementById("regex");
 let after = document.getElementById("after");
 
-// let regex = /[^hl]+/g
+let regex;
 // formula.value = regex;
 
 before.addEventListener("input", forwardConvert);
@@ -12,33 +12,46 @@ formula.addEventListener("input", updateRegex);
 after.addEventListener("input", backwardConvert);
 
 function forwardConvert(e) {
-    console.log(before.value.match(regex))
-        if (regex.test(before.value)) {
-        after.value = before.value.match(regex).join("");
-    } else {
-        after.value = "";
+    //validate regex first
+    try {regex.test(before.value)}
+    catch(err) {
+        return;
     }
+
+    // after.value = before.value.replace(regex, "");
+    after.value = before.value.match(regex).join("");
+    
+    // after.value = regex.exec(before.value);
 }
+
+//include split, replace, match, search modes
 
 function updateRegex(e) {
     //validate regex first
-    try {RegExp(e.target.value,"g")}
+    try {RegExp(e.target.value, "g")}
     catch(err) {
+        // console.log(err)
         formula.style.color = "red";
+        after.style.color = "red";
         return;
     }
     //it's valid, so continue
-    regex = RegExp(e.target.value,"g");
+    regex = RegExp(e.target.value, "g");
     formula.style.color = "black";
+    after.style.color = "black";
 
     //use empty string if null result
     if (regex.test(before.value)) {
+        // after.value = before.value.replace(regex, "");
         after.value = before.value.match(regex).join("");
-    } else {
-        after.value = "";
-    }
 
-    console.log(after.value)
+        // console.log(before.value.replace(regex, ""))
+        // after.value = regex.exec(before.value);
+        // console.log(before.value.match(regex))
+    } 
+    // else {
+    //     after.value = "";
+    // }
 }
 
 function backwardConvert(e) {
